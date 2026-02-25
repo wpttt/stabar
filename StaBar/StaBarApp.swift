02752701a446e4b1a5c0aec713157659c8d09587
Wang Pt <wpttt@msn.cn>
2026-02-25 20:42:38 +0800

    chore: initial Xcode project setup

---
+++ StaBar/StaBarApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct StaBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StaBarTests/StaBarTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+
+final class StaBarTests: XCTestCase {
+    
+    func testExample() throws {
+        XCTAssert(true)
+    }
+}
